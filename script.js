d1ad317cb4645018d86318911bba7194d277e544
jimi <jamalmoosvi@gmail.com>
2021-08-04 18:32:05 +0500

    added graana in experience

---
+++ script.js
@@ -0,0 +1,7 @@
+let currentDate = new Date("2015/03/25")
+console.log(currentDate)
+currentDate = JSON.stringify(currentDate)
+console.log(typeof(currentDate))
+currentDateSplitted = currentDate.split(' ')
+const startDate = 'Wed Aug 04 2021 14:40:02 GMT+0500 (Pakistan Standard Time)'
+console.log(currentDateSplitted[0])
